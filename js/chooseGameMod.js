674cc59d353557cdefa9373b6f1775b2c3c41c53
Alexsandre Akofovi <52098@etu.he2b.be>
2022-11-04 17:20:25 +0100

    css modification, new page with new mods

--- js/chooseGameMod.js
+++ js/chooseGameMod.js
@@ -1,10 +1,28 @@
+let pseudoPlayer1 = new URL(document.location.href).searchParams.get("playerOnePseudo");
+let pseudoPlayer2 = new URL(document.location.href).searchParams.get("playerTwoPseudo");
+
+let tab = [pseudoPlayer1,pseudoPlayer2];
+
 /**
  * This function takes the players' nicknames in the url and displays it on the screen 
  */
 function DisplayPlayersPseudo() {
-    let pseudoPlayer1 = new URL(document.location.href).searchParams.get("playerOnePseudo");
-    let pseudoPlayer2 = new URL(document.location.href).searchParams.get("playerTwoPseudo");
+    for (let i = 0; i < tab.length; ++i) {
+        create_the_elements_and_nodes(tab[i]);
+    }
+}
 
-    $("section header").append("<p>"+pseudoPlayer1+"</p>");
-    $("section header").append("<p>"+pseudoPlayer2+"</p>");
+
+/**
+ * This function create and add the nicknames in the element (section id=pseudos)
+ * @param {String} data 
+ */
+function create_the_elements_and_nodes(data){
+
+    let the_place_for_the_element = document.getElementById("pseudos");
+    let new_p = document.createElement("p");
+    let p_node = document.createTextNode(data);
+
+    new_p.appendChild(p_node);
+    the_place_for_the_element.appendChild(new_p);
 }
