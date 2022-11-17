bd8962b58e50636b85dc926f31abd35cb1f749fd
Alexsandre Akofovi <52098@etu.he2b.be>
2022-11-17 14:30:28 +0100

    classicMod ok but need to mofidy css and js

--- js/classicMod.js
+++ js/classicMod.js
@@ -1,5 +1,23 @@
+function toSend(){
+    let addTo = document.getElementById("toSendResultPage");
+    let newInput = document.createElement("input");
+    newInput.setAttribute("type", "hidden");
+    newInput.setAttribute("name", "pseudoPlayerOne");
+    newInput.setAttribute("value", tab[0]);
+    addTo.appendChild(newInput);
+    
+    addTo = document.getElementById("toSendResultPage");
+    newInput = document.createElement("input");
+    newInput.setAttribute("type", "hidden");
+    newInput.setAttribute("name", "pseudoPlayerTwo");
+    newInput.setAttribute("value", tab[2]);
+    addTo.appendChild(newInput);
+}
+
+toSend();
+
 /**
- * This function display all characters 
+ * This function display all characters for the first player
  */
 function displayAllCharactersP1(i) {
     let newImg;
@@ -10,6 +28,8 @@ function displayAllCharactersP1(i) {
     setTimeout(function () {
         newImg = document.createElement("img");
         newImg.setAttribute("src", characters[i].picture);
+        newImg.setAttribute("id", "F" + characters[i].identifier);
+        newImg.setAttribute("onclick", 'chooseP1(this)');
         id = document.getElementById("allCharactersP1");
         id.appendChild(newImg);
         displayAllCharactersP1(++i);
@@ -18,6 +38,9 @@ function displayAllCharactersP1(i) {
 
 displayAllCharactersP1(0);
 
+/**
+ * This function display all characters for the second player
+ */
 function displayAllCharactersP2(i) {
     let newImg;
     let id;
@@ -27,7 +50,9 @@ function displayAllCharactersP2(i) {
     setTimeout(function () {
         newImg = document.createElement("img");
         newImg.setAttribute("src", characters[i].picture);
+        newImg.setAttribute("id", "S" + characters[i].identifier);
         id = document.getElementById("allCharactersP2");
+        newImg.setAttribute("onclick", 'chooseP2(this)');
         id.appendChild(newImg);
         displayAllCharactersP2(++i);
     }, 50);
@@ -35,26 +60,62 @@ function displayAllCharactersP2(i) {
 
 displayAllCharactersP2(0);
 
-
+/**
+ * This function create a image and add the pseudos
+ */
 function addThePlayer() {
-    let the_place_for_the_element = document.getElementById("playerOneCharacterChoose");
+    let thePlaceForTheElement = document.getElementById("playerOne");
+    let thePlaceForTheElement2 = document.getElementById("playerOneCharacterChoose");
     let pNode = document.createTextNode(tab[0]);
-    sameThings(the_place_for_the_element,pNode);
+    sameThings(thePlaceForTheElement, thePlaceForTheElement2, pNode);
 
-    the_place_for_the_element = document.getElementById("playerTwoCharacterChoose");
+    thePlaceForTheElement = document.getElementById("playerTwo");
+    thePlaceForTheElement2 = document.getElementById("playerTwoCharacterChoose");
     pNode = document.createTextNode(tab[2]);
-    sameThings(the_place_for_the_element,pNode);
+    sameThings(thePlaceForTheElement, thePlaceForTheElement2, pNode);
 }
 
-function sameThings(the_place_for_the_element,pNode){
+/**
+ * This function use the same caracters for the function "addThePlayer()"
+ */
+function sameThings(thePlaceForTheElement, thePlaceForTheElement2, pNode) {
     let newP = document.createElement("p");
 
     let newImg = document.createElement("img");
     newImg.setAttribute("src", "../source/question-mark-1544553868vD2.jpg");
 
     newP.appendChild(pNode);
-    the_place_for_the_element.appendChild(newP);
-    the_place_for_the_element.appendChild(newImg);
+    thePlaceForTheElement.appendChild(newP);
+    thePlaceForTheElement2.appendChild(newImg);
+    addTheCompter(thePlaceForTheElement);
 }
 
 addThePlayer();
+
+function chooseP1(characterPicture) {
+    let select = document.getElementById("playerOneCharacterChoose");
+    removeAndAdd(select, characterPicture);
+}
+
+function chooseP2(characterPicture) {
+    let select = document.getElementById("playerTwoCharacterChoose");
+    removeAndAdd(select, characterPicture);
+}
+
+
+function removeAndAdd(select, characterPicture) {
+    select.removeChild(select.lastChild);
+
+    let newImg = document.createElement("img");
+    newImg.setAttribute("src", characterPicture.src);
+    select.appendChild(newImg);
+}
+
+function addTheCompter(select) {
+    let newInput = document.createElement("input");
+    newInput.setAttribute("type", "number");
+    newInput.setAttribute("value", 0);
+    newInput.setAttribute("min", 0);
+    select.appendChild(newInput);
+
+}
